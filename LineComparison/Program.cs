36d655ec20f44e08739d1f7ab9a9a2515f31021e
AnshumanMunmun <imanshuman47@gmail.com>
2022-09-01 00:05:54 +0530

    [Anshuman] Added: Adding Method of Equals and CompareTo

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -9,15 +9,17 @@ namespace LineComparisionProblem
             Comparison comparison = new Comparison(4, 5, 6, 7);
             double line1 = comparison.CalcLength();
             Console.WriteLine("Length of 1st Line: " + line1);
-            Comparison linecomparison = new Comparison(8, 9, 11, 12);
+            Comparison linecomparison = new Comparison(8, 9, 10, 12);
             double line2 = linecomparison.CalcLength();
             Console.WriteLine("Length of 2nd Line: " + line2);
-            if (line1.CompareTo(line2) == 0)
+            if (line1.Equals(line2))
                 Console.WriteLine("Both Lines are Equal");
-            if (line1.CompareTo(line2) > 0)
-                Console.WriteLine("1st Line is Greater than 2nd Line");
-            else
-                Console.WriteLine("2nd Line is Greater than 1st Line");
+            {
+                if (line1.CompareTo(line2) > 0)
+                    Console.WriteLine("1st Line is Greater than 2nd Line");
+                else
+                    Console.WriteLine("2nd Line is Greater than 1st Line");
+            }
         }
     }
 }
